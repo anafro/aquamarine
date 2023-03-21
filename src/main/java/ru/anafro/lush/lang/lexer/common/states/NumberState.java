f24f2905575771007e1aa2315f565f6cb17f4594
Anatoly Frolov <contact@anafro.ru>
2023-03-21 12:15:24 +1000

    Made empty chars ignored

--- src/main/java/ru/anafro/lush/lang/lexer/common/states/NumberState.java
+++ src/main/java/ru/anafro/lush/lang/lexer/common/states/NumberState.java
@@ -7,7 +7,7 @@ import ru.anafro.lush.lang.lexer.common.tokens.NumberLiteralToken;
 
 public class NumberState extends LexerState {
     public NumberState(Lexer lexer) {
-        super(lexer, false);
+        super(lexer, true);
     }
 
     @Override
@@ -15,6 +15,12 @@ public class NumberState extends LexerState {
         if (NumericState.isCharCorrect(character) || character == NumberLiteralToken.FLOATING_PART_SEPARATOR) {
             lexer.appendCurrentCharacterToBuffer();
         } else {
+            if (IdState.isCharCorrect(character)) {
+                lexer.addError("We found a letter right after the number. It's probably just a typo, so please remove that symbol '%c'.".formatted(
+                        character
+                ));
+            }
+
             if (lexer.getBufferContent().endsWith(String.valueOf(NumberLiteralToken.FLOATING_PART_SEPARATOR))) {
                 lexer.addCosmeticProblem("A fractional part of the number is empty. You probably made a typo. If you wanted to make a number with a fractional part equals to zero, please, use .0 like so: 14.0");
             }
